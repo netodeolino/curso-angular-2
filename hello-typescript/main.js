294cebf5ee507c7d91f432b4e955eb05407c9968
Neto Deolino <netodeolino@gmail.com>
2017-03-22 23:13:26 -0300

    22-03-2017

---
+++ hello-typescript/main.js
@@ -0,0 +1,6 @@
+var teste = 'asdasdasd';
+function minha(x, y) {
+    return x * y;
+}
+var tres = 3;
+var PI = 3.14;
---
+++ hello-typescript/main.ts
@@ -0,0 +1,8 @@
+var teste = 'asdasdasd';
+
+function minha (x, y) {
+    return x*y;
+}
+
+let tres = 3;
+const PI = 3.14;
