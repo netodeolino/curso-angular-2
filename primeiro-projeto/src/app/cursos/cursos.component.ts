d677a5592983b4702ddda8d469ae5d309ec0cb62
Neto Deolino <netodeolino@gmail.com>
2017-03-27 19:47:01 -0300

    27-03-2017

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: string[];
+
+  constructor() { 
+    this.nomePortal = 'netodeolino@gmail.com';
+  }
 
   ngOnInit() {
   }
